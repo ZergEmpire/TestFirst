4f437ff63b11e4a1c25cdb32cac1d7e25a0eb253
MessirVoland <messir-voland@mail.ru>
2021-08-06 17:12:35 +0300

    123

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -5,23 +5,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CommandExecutor;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class First{
 
     @Test
     public void test(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        WebDriver driver = new ChromeDriver(options);;
         //System.setProperty("chromeoptions.args", "\"--no-sandbox\",\"--disable-dev-shm-usage\"");
-
-
-        Selenide.open("https://tokapizza.ru/");
-        Selenide.$(By.xpath("//a[@href=\"/menu/pizza\"]")).click();
-        //Selenide.$(By.xpath("//div[contains(@class, \"section-title\")]")).shouldHave(Condition.enabled).shouldHave(Condition.exactText("ХУЙ"));
+        open("https://tokapizza.ru/");
+        $(By.xpath("//a[@href=\"/menu/pizza\"]")).click();
+        //$(By.xpath("//div[contains(@class, \"section-title\")]")).shouldHave(Condition.enabled).shouldHave(Condition.exactText("ХУЙ"));
 
         System.out.println();
     }
+
 
 }
