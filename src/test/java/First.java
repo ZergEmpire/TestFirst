f4dd111d7786a865eb2eeeec4dde55267ac9406a
ZergEmpire <sergeyzergempire@gmail.com>
2021-08-09 11:10:05 +0300

    Correct Pipeline

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -34,7 +34,7 @@ public class First{
         ));
         try {
             RemoteWebDriver driver = new RemoteWebDriver(
-                    URI.create("http://selenoid:4444/wd/hub").toURL(),
+                    URI.create("http://192.168.1.17:8080/wd/hub").toURL(),
                     capabilities
             );
         } catch (MalformedURLException e) {
