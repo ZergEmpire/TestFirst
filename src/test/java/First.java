0036026220d1ab4727da7f93d6fe40ca9a67fa81
ZergEmpire <sergeyzergempire@gmail.com>
2021-08-09 10:43:50 +0300

    Correct Pipeline

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.apache.commons.exec.OS;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -7,6 +9,18 @@ public class First{
 
     @Test
     public void test(){
+
+        if (OS.isFamilyUnix()) {
+            Configuration.headless = true;
+            System.setProperty("chromeoptions.args", "\"--no-sandbox\"");
+            System.setProperty("chromeoptions.args", "\"--disable-dev-shm-usage\"");
+            System.setProperty("chromeoptions.args", "--disable-gpu");
+            System.setProperty("chromeoptions.args", "--headless");
+            System.setProperty("chromeoptions.args", "--disable-setuid-sandbox");
+            System.setProperty("chromeoptions.args", "--test-type");
+            System.setProperty("webdriver.chrome.whitelistedIps", "");
+        }
+
         Selenide.open("https://tokapizza.ru/");
         Selenide.$(By.xpath("//a[@href=\"/menu/pizza\"]")).click();
         Selenide.$(By.xpath("//div[contains(@class, \"section-title\")]")).shouldHave(Condition.enabled).shouldHave(Condition.exactText("ХУЙ"));
